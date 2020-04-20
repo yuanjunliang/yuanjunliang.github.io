3426aaae02ddec098da1ec2ed88215671ce0b61a
yuanjunliang <1610712512@qq.com>
2020-04-20 21:54:47 +0800

    update baseurl

--- src/plugin/axios.js
+++ src/plugin/axios.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/static/mock' : 'https://yuanjunliang.github.io/docs'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/static/mock' : 'http://yuanjunliang.cn/docs'
 
 export default Axios.create({
   baseURL: baseURL
