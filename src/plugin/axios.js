369a586f669006e3d978c8d9e63dc9baafedd422
yuanjunliang <1610712512@qq.com>
2020-04-28 11:51:16 +0800

    update axios plugin

--- src/plugin/axios.js
+++ src/plugin/axios.js
@@ -1,8 +1,8 @@
 // import Axios from 'axios'
 // const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/static/mock' : 'http://yuanjunliang.cn/docs'
-// const baseURL = window.location.host === 'yuanjunliang.cn' ? 'http://yuanjunliang.cn/docs' : 'http://localhost:8080/static/mock'
+const baseURL = window.location.host === 'yuanjunliang.cn' ? 'http://yuanjunliang.cn/docs' : window.config.baseURL
 
-const baseURL = window.config.baseURL || 'http://localhost:8080/static/mock'
+// const baseURL = window.config.baseURL || 'http://localhost:8080/static/mock'
 console.log({baseURL})
 export default axios.create({
   baseURL: baseURL
--- static/config.js
+++ static/config.js
@@ -1,6 +1,6 @@
 window.config = {
   blogName: '袁俊亮技术博客博客',
-  // baseURL: 'http://yuanjunliang.cn/docs',
+  baseURL: 'http://localhost:8080/static/mock',
   ads: {
     header: '<a href="https://www.vultr.com/?ref=8546888"><img src="https://www.vultr.com/media/banners/banner_728x90.png" width="728" height="90"></a>',
     sidebar: ''
